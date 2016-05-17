1347adaa94367484d28b78cac69d29583f682ecf
ashutosh <ashutosh.jha@happay.in>
2016-05-17 16:55:39 +0530

    todo application implemented using nsfectchresults controller

--- NSFetchResultController/AddToDoViewController.swift
+++ NSFetchResultController/AddToDoViewController.swift
@@ -7,17 +7,51 @@
 //
 
 import UIKit
+import CoreData
 
 class AddToDoViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var textField: UITextField?
+    
+    var managedObjectContext: NSManagedObjectContext!
     
     @IBAction func cancel(sender: AnyObject) {
         dismissViewControllerAnimated(true, completion: nil)
     }
     
     @IBAction func save(sender: AnyObject) {
-        dismissViewControllerAnimated(true, completion: nil)
+        let name = textField!.text
+        
+        if let isEmpty = name?.isEmpty where isEmpty == false {
+            // Create Entity
+            let entity = NSEntityDescription.entityForName("Item", inManagedObjectContext: self.managedObjectContext)
+            
+            // Initialize Record
+            let record = NSManagedObject(entity: entity!, insertIntoManagedObjectContext: self.managedObjectContext)
+            
+            // Populate Record
+            record.setValue(name, forKey: "name")
+            record.setValue(NSDate(), forKey: "createdAt")
+            
+            do {
+                // Save Record
+                try record.managedObjectContext?.save()
+                
+                // Dismiss View Controller
+                dismissViewControllerAnimated(true, completion: nil)
+                
+            } catch {
+                let saveError = error as NSError
+                print("\(saveError), \(saveError.userInfo)")
+                
+                // Show Alert View
+                showAlertWithTitle("Warning", message: "Your to-do could not be saved.", cancelButtonTitle: "OK")
+            }
+            
+        } else {
+            // Show Alert View
+            showAlertWithTitle("Warning", message: "Your to-do needs a name.", cancelButtonTitle: "OK")
+        }
     }
 
     override func viewDidLoad() {
@@ -31,15 +65,13 @@ class AddToDoViewController: UIViewController, UITextFieldDelegate {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func showAlertWithTitle(title: String, message: String, cancelButtonTitle: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .Alert)
+        
+        alertController.addAction(UIAlertAction(title: cancelButtonTitle, style: .Default, handler: nil))
+        
+        presentViewController(alertController, animated: true, completion: nil)
+    
     }
-    */
 
 }
--- NSFetchResultController/ViewController.swift
+++ NSFetchResultController/ViewController.swift
@@ -53,13 +53,47 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     
     // MARK: Table View Data Source Methods
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let sections = fetchedResultsController.sections {
+            let sectionInfo = sections[section]
+            return sectionInfo.numberOfObjects
+        }
+        
         return 0
+    }
+    
+    func configureCell(cell: ToDoCell, atIndexPath indexPath: NSIndexPath) {
+        let records = fetchedResultsController.objectAtIndexPath(indexPath)
+        
+        if let name = records.valueForKey("name") as? String {
+            cell.nameLabel?.text = name
+        }
+        
+        if let done = records.valueForKey("done") as? Bool {
+            cell.doneButton?.selected = done
+        }
+        
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier(ReuseIdentifierToDoCell, forIndexPath: indexPath) as! ToDoCell
         
+        configureCell(cell, atIndexPath: indexPath)
         return cell
+    }
+    
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        
+        if editingStyle == .Delete {
+            let managedObject:NSManagedObject = fetchedResultsController.objectAtIndexPath(indexPath) as! NSManagedObject
+            managedObjectContext?.deleteObject(managedObject)
+            do {
+                try managedObjectContext?.save()
+                tableView.reloadData()
+            } catch {
+                let error = error as NSError
+                print("\(error)")
+            }
+        }
     }
     
     // MARK: Fetch Results Controller Delegates methods
@@ -98,10 +132,29 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
                 tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
             }
             break
-        default:
-            <#code#>
         }
     }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        if let sections = fetchedResultsController.sections {
+            return sections.count
+        }
+        
+        return 0
+    }
+    
+    
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "SegueAddToDoViewController" {
+            if let navigationController = segue.destinationViewController as? UINavigationController {
+                if let viewController = navigationController.topViewController as? AddToDoViewController {
+                    viewController.managedObjectContext = managedObjectContext
+                }
+            }
+        }
+    }
+    
 
 }
 
